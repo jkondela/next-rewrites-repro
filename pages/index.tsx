ae7bbafaadcc01a6e68c7c27622d330156ed2d8e
Jaro Kondela <jaqkit@hotmail.sk>
2024-01-08 15:21:14 +0100

    Add books example

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,5 +2,5 @@ import Link from "next/link";
 
 /** Add your relevant code here for the issue to reproduce */
 export default function Home() {
-  return <Link href="/books/1">Go to product 1</Link>;
+  return <Link href="/books">Go to products</Link>;
 }
